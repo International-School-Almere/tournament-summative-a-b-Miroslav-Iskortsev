715242421d9dc00216bcb4282af7002225b86cb7
IskoMir-web <136828@secondary.isalmere.nl>
2026-03-31 10:43:06 +0200

    Make a TK inter screen and finish load and save json

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
 import json
 import os
 
@@ -7,3 +10,49 @@ def load_data():
         return {"teams": [], "individuals": [], "events": []}
     with open(file, "r", encoding="utf-8") as f:
         return json.load(f) 
+    
+def save_data(data):
+    with open("data.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
+root = tk.Tk()
+root.title("Tournament Management System")
+root.geometry("800x870")
+
+
+# def show_participants_screen():
+
+
+# def show_events_screen():
+
+
+# def show_leaderboard_screen():
+
+
+# def make_participants_table():
+
+
+# def update_participants_data():
+
+
+# def delete_entry(item_id):
+
+
+# def make_events_list():
+
+
+# def add_new_event():
+
+
+# def save_event_changes(event_id):
+
+
+# def calculate_team_scores():
+
+
+# def get_sorted_leaderboard():
+
+
+# def confirm_action(message):
+
+root.mainloop()
