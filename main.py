0b03045b428a502d4f408bd8c94d4f1f597d7639
IskoMir-web <136828@secondary.isalmere.nl>
2026-03-31 10:26:01 +0200

    Adding json load in code

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-#main file for the Tournament App.
-#test
+import json
+import os
+
+def load_data():
+    file = "data.json"
+    if not os.path.exists(file):
+        return {"teams": [], "individuals": [], "events": []}
+    with open(file, "r", encoding="utf-8") as f:
+        return json.load(f) 
