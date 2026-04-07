60a5fe690a6f265cde15f0615d79000b180b62dd
IskoMir-web <136828@secondary.isalmere.nl>
2026-04-07 10:28:19 +0200

    Refactor main.py to organize GUI initialization and improve code structure

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from logging import root
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import json
 import os
 
+root = tk.Tk()
+# Load and save data functions
 def load_data():
     file = "data.json"
     if not os.path.exists(file):
@@ -14,11 +17,19 @@ def load_data():
 def save_data(data):
     with open("data.json", "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
+# GUI functions
 
-root = tk.Tk()
-root.title("Tournament Management System")
-root.geometry("800x870")
-
+class TournamentApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Tournament Management System")
+        self.root.geometry("800x870")
+    def main_screen(self):    
+        self.root.mainloop()
+    def show_participants_screen(self):
+        self.label_leaderboard = tk.Label(self.root, text="Participants", font=("Arial", 16))
+        self.label_leaderboard.pack(pady=10)
+    
 
 # def show_participants_screen():
 
@@ -26,7 +37,7 @@ root.geometry("800x870")
 # def show_events_screen():
 
 
-# def show_leaderboard_screen():
+# def show_leaderboard_screen(): # add classes
 
 
 # def make_participants_table():
@@ -53,6 +64,13 @@ root.geometry("800x870")
 # def get_sorted_leaderboard():
 
 
-# def confirm_action(message):
+def confirm_action(message):
+    return messagebox.askyesno("Confirmation", message)
 
-root.mainloop()
+
+
+
+TournamentApp(root)
+
+TournamentApp(root).show_participants_screen()
+TournamentApp(root).main_screen()
