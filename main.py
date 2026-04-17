21caa3e52e9f66e36f1e660da82f00f4d26475e8
IskoMir-web <136828@secondary.isalmere.nl>
2026-04-17 17:40:47 +0200

    Finishing 90% of the app, filling progress.md in that version app works

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 from tkinter import messagebox
 import json
 import os
+import datetime
 
 root = tk.Tk()
 # Load and save data functions
@@ -24,6 +25,9 @@ class TournamentApp:
         self.root = root
         self.root.title("Tournament Management System")
         self.root.geometry("1250x870")
+        
+        #load data when system start
+        self.data = load_data()
 
         self.label_leaderboard = tk.Label(self.root, text="Participants", font=("Arial", 16))
         self.label_leaderboard.pack(pady=10)
@@ -43,6 +47,9 @@ class TournamentApp:
         self.main=tk.Frame(self.root)
         self.main.pack(fill="both", expand=True)
         self.main.config(bg="#f0f0f0", padx=20, pady=20)
+        
+        #Open event screen already after system start
+        self.show_overview_screen()
 
     def main_loop(self):    
         self.root.mainloop()
@@ -71,6 +78,13 @@ class TournamentApp:
         self.label_team_score = tk.Label(self.team_rank_frame, text="Score", font=("Arial", 10, "bold"))
         self.label_team_score.grid(row=0, column=2)
 
+        #Sorting teams by highest score
+        sorted_teams = sorted(self.data.get("teams", []), key=lambda x: int(x.get("points", 0)), reverse=True)
+        for i, team in enumerate(sorted_teams[:10]):
+            tk.Label(self.team_rank_frame, text=str(i+1), font=("Arial", 10)).grid(row=i+1, column=0, pady=2)
+            tk.Label(self.team_rank_frame, text=team["name"], font=("Arial", 10)).grid(row=i+1, column=1, pady=2)
+            tk.Label(self.team_rank_frame, text=str(team["points"]), font=("Arial", 10)).grid(row=i+1, column=2, pady=2)
+
         #Indiv ranking
         self.indiv_rank_frame = tk.LabelFrame(self.tables_container, text=" Individual Standings ", font=("Arial", 13, "bold"), padx=10, pady=10)
         self.indiv_rank_frame.pack(side="left", fill="both", expand=True, padx=10)
@@ -82,12 +96,19 @@ class TournamentApp:
         self.label_indiv_score = tk.Label(self.indiv_rank_frame, text="Score", font=("Arial", 10, "bold"))
         self.label_indiv_score.grid(row=0, column=2)
 
+        #Sorting indiv paricip by highest score
+        sorted_indivs = sorted(self.data.get("individuals", []), key=lambda x: int(x.get("points", 0)), reverse=True)
+        for i, indiv in enumerate(sorted_indivs[:10]):
+            tk.Label(self.indiv_rank_frame, text=str(i+1), font=("Arial", 10)).grid(row=i+1, column=0, pady=2)
+            tk.Label(self.indiv_rank_frame, text=indiv["name"], font=("Arial", 10)).grid(row=i+1, column=1, pady=2)
+            tk.Label(self.indiv_rank_frame, text=str(indiv["points"]), font=("Arial", 10)).grid(row=i+1, column=2, pady=2)
+
         self.show_events_screen()
 
     #upcoming events
     def show_events_screen(self):
         
-        self.title_label = tk.Label(self.main, text="UPCOMING EVENTS", font=("Arial", 17, "bold"))
+        self.title_label = tk.Label(self.main, text="Upcoming events", font=("Arial", 17, "bold"))
         self.title_label.pack(pady=20)
 
         self.events_frame = tk.Frame(self.main)
@@ -105,7 +126,8 @@ class TournamentApp:
         self.event1_date.grid(row=1, column=1, padx=10, pady=5)
 
         tk.Label(self.event1_box, text="Type:").grid(row=2, column=0, sticky="w")
-        ttk.Combobox(self.event1_box, values=["Team", "Individual"]).grid(row=2, column=1)
+        self.event1_type = ttk.Combobox(self.event1_box, values=["Team", "Individual"])
+        self.event1_type.grid(row=2, column=1)
 
         self.event2_box = tk.LabelFrame(self.events_frame, text=" Event #2 ", padx=15, pady=15)
         self.event2_box.pack(fill="x", pady=10)
@@ -119,7 +141,32 @@ class TournamentApp:
         self.event2_date.grid(row=1, column=1, padx=10, pady=5)
 
         tk.Label(self.event2_box, text="Type:").grid(row=2, column=0, sticky="w")
-        ttk.Combobox(self.event2_box, values=["Team", "Individual"]).grid(row=2, column=1)
+        self.event2_type = ttk.Combobox(self.event2_box, values=["Team", "Individual"])
+        self.event2_type.grid(row=2, column=1)
+
+        #determine the data andб sorting events by date and putting them in right order to overview screen
+        parsed_events = []
+        for ev in self.data.get("events", []):
+            dt = None
+            for fmt in ("%Y-%m-%d", "%d.%m.%Y", "%d/%m/%Y"): 
+                try:
+                    dt = datetime.datetime.strptime(ev["date"], fmt)
+                    break
+                except: pass
+            if not dt: dt = datetime.datetime.max #to avoid errors
+            parsed_events.append((dt, ev)) #sort events by time
+            
+        parsed_events.sort(key=lambda x: x[0])
+        upcoming = [e[1] for e in parsed_events] #put events in right order
+#put events to overview screen
+        if len(upcoming) > 0:
+            self.event1_name.insert(0, upcoming[0].get("description", ""))
+            self.event1_date.insert(0, upcoming[0].get("date", ""))
+            self.event1_type.set(upcoming[0].get("type", ""))
+        if len(upcoming) > 1:
+            self.event2_name.insert(0, upcoming[1].get("description", ""))
+            self.event2_date.insert(0, upcoming[1].get("date", ""))
+            self.event2_type.set(upcoming[1].get("type", ""))
 
 
     def show_event_screen(self):
@@ -144,9 +191,13 @@ class TournamentApp:
         btn_frame = tk.Frame(self.main)
         btn_frame.pack(fill="x", side="bottom")
 
-        tk.Button(btn_frame, text="Add Event", command=self.add_event_box).pack(side="left", padx=5)
-        tk.Button(btn_frame, text="Delte").pack(side="right", padx=5)
-        tk.Button(btn_frame, text="Update").pack(side="right", padx=5)
+        tk.Button(btn_frame, text="Add Event", command=lambda: self.add_event_box()).pack(side="left", padx=5)
+        tk.Button(btn_frame, text="Delete", command=self.delete_event_box).pack(side="right", padx=5)
+        tk.Button(btn_frame, text="Update", command=self.save_events).pack(side="right", padx=5)
+
+        #Put created events to the events screen
+        for ev in self.data.get("events", []):
+            self.add_event_box(ev)
 
 
     def show_participants_screen(self):
@@ -155,7 +206,7 @@ class TournamentApp:
         self.main = tk.Frame(self.root)
         self.main.pack(fill="both", expand=True)
 
-        tk.Label(self.main, text="PAticipants", font=("Arial", 18, "bold")).pack(pady=10)
+        tk.Label(self.main, text="Participants", font=("Arial", 18, "bold")).pack(pady=10)
 
         wrap = tk.Frame(self.main)
         wrap.pack(fill="both", expand=True)
@@ -227,6 +278,26 @@ class TournamentApp:
         tk.Button(team, text="Update", command=self.update_team).pack(side="left", padx=5)
         tk.Button(team, text="Delete", command=self.delete_team).pack(side="left", padx=5)
 
+        #fillin tables after reopen particip page
+        for i, indiv_data in enumerate(self.data.get("individuals", [])):
+            self.indiv_tree.insert("", "end", values=(i+1, indiv_data["name"], indiv_data["points"]))
+        for i, team_data in enumerate(self.data.get("teams", [])):
+            self.team_tree.insert("", "end", values=(i+1, team_data["name"], team_data.get("p1",""), team_data.get("p2",""), team_data.get("p3",""), team_data.get("p4",""), team_data.get("p5",""), team_data["points"]))
+
+ #get data from particip treeview and save to json
+    def save_participants_state(self):
+        individuals = []
+        for item in self.indiv_tree.get_children():
+            vals = self.indiv_tree.item(item, "values")
+            individuals.append({"name": vals[1], "points": int(vals[2] or 0)})
+        teams = []
+        for item in self.team_tree.get_children():
+            vals = self.team_tree.item(item, "values")
+            teams.append({"name": vals[1], "p1": vals[2], "p2": vals[3], "p3": vals[4], "p4": vals[5], "p5": vals[6], "points": int(vals[7] or 0)})
+        self.data["individuals"] = individuals
+        self.data["teams"] = teams
+        save_data(self.data)
+
 #Add individuals
     def add_individual(self):
         name = self.indiv_name_entry.get()
@@ -234,6 +305,7 @@ class TournamentApp:
         if name:
             count = len(self.indiv_tree.get_children()) + 1
             self.indiv_tree.insert("", "end", values=(count, name, points))
+            self.save_participants_state()
 
 #adding team with team participants and points for team
     def add_team(self):
@@ -241,6 +313,8 @@ class TournamentApp:
         if team:
             count = len(self.team_tree.get_children()) + 1
             self.team_tree.insert("", "end", values=(count, team, self.p1.get(), self.p2.get(), self.p3.get(), self.p4.get(), self.p5.get(), self.team_points.get() or 0))
+            self.save_participants_state()
+
     #adding points for indiv particip
     def add_points_indiv(self):
         sel = self.indiv_tree.selection()
@@ -250,6 +324,8 @@ class TournamentApp:
             vals = list(item["values"])
             vals[2] = int(vals[2]) + add
             self.indiv_tree.item(sel, values=vals)
+            self.save_participants_state()#save to json without confirmation
+
     #adding points for teams
     def add_points_team(self):
         sel = self.team_tree.selection()
@@ -259,6 +335,7 @@ class TournamentApp:
             vals = list(item["values"])
             vals[7] = int(vals[7]) + add
             self.team_tree.item(sel, values=vals)
+            self.save_participants_state()
 
     #updating indiv particip
     def update_indiv(self):
@@ -268,55 +345,86 @@ class TournamentApp:
             points = self.indiv_points.get()
             item = self.indiv_tree.item(sel)
             vals = list(item["values"])
-            if name:
-                vals[1] = name
-            if points:
-                vals[2] = points
+            if name: vals[1] = name
+            if points: vals[2] = points
             self.indiv_tree.item(sel, values=vals)
+            self.save_participants_state()
+
     #Updatin teams info
     def update_team(self):
         sel = self.team_tree.selection()
         if sel:
             item = self.team_tree.item(sel)
             vals = list(item["values"])
-
-            if self.team_name_entry.get():
-                vals[1] = self.team_name_entry.get()
+            if self.team_name_entry.get(): vals[1] = self.team_name_entry.get()
             vals[2] = self.p1.get() or vals[2]
             vals[3] = self.p2.get() or vals[3]
             vals[4] = self.p3.get() or vals[4]
             vals[5] = self.p4.get() or vals[5]
             vals[6] = self.p5.get() or vals[6]
-
-            if self.team_points.get():
-                vals[7] = self.team_points.get()
-
+            if self.team_points.get(): vals[7] = self.team_points.get()
             self.team_tree.item(sel, values=vals)
+            self.save_participants_state()
+
     #deleating indiv particip
     def delete_indiv(self):
         for i in self.indiv_tree.selection():
             self.indiv_tree.delete(i)
+        self.save_participants_state()
+
     #deleating team
     def delete_team(self):
         for i in self.team_tree.selection():
             self.team_tree.delete(i)
+        self.save_participants_state()
 
-    #Adding event boxes for new events
-    def add_event_box(self):
+
+# Adding event boxes for new events
+    def add_event_box(self, event_data=None):
         box = tk.LabelFrame(self.scroll_frame, text="Event")
         box.pack(fill="x", padx=10, pady=5)
 
+        #delete button for event box
+        btn_del = tk.Button(box, text=" X ", fg="red", command=lambda: [box.destroy(), self.save_events()])
+        btn_del.grid(row=0, column=4, padx=5, sticky="ne")
+        #date of event
         tk.Label(box, text="Date:").grid(row=0, column=0)
-        tk.Entry(box).grid(row=0, column=1)
-
+        ent_date = tk.Entry(box); ent_date.grid(row=0, column=1)
+        #type of event
         tk.Label(box, text="Type:").grid(row=0, column=2)
-        ttk.Combobox(box, values=["Team", "Individual"], width=10).grid(row=0, column=3)
-
+        cb_type = ttk.Combobox(box, values=["Team", "Individual"], width=10); cb_type.grid(row=0, column=3)
+    #desc of event
         tk.Label(box, text="Description:").grid(row=1, column=0)
-        tk.Entry(box).grid(row=1, column=1, columnspan=3)
-
+        ent_desc = tk.Entry(box); ent_desc.grid(row=1, column=1, columnspan=3)
+        #partip of events
         tk.Label(box, text="Participants:").grid(row=2, column=0)
-        tk.Entry(box).grid(row=2, column=1, columnspan=3)
+        ent_part = tk.Entry(box); ent_part.grid(row=2, column=1, columnspan=3)
+        
+        #saving data properly
+        box.widgets = (ent_date, cb_type, ent_desc, ent_part)
+        if event_data:
+            ent_date.insert(0, event_data.get("date", ""))
+            cb_type.set(event_data.get("type", ""))
+            ent_desc.insert(0, event_data.get("description", ""))
+            ent_part.insert(0, event_data.get("participants", ""))
+
+    #gathering data from ev boxes and saving to json
+    def save_events(self):
+        self.data["events"] = []
+        for box in self.scroll_frame.winfo_children():
+            if isinstance(box, tk.LabelFrame) and hasattr(box, 'widgets'):
+                d, t, desc, p = box.widgets
+                self.data["events"].append({"date": d.get(), "type": t.get(), "description": desc.get(), "participants": p.get()})
+        save_data(self.data)
+        messagebox.showinfo("Saved", "Events updated")
+
+    #deleating last in list event box with confirmation
+    def delete_event_box(self):
+            if confirm_action("Are you sure you want to delete the last event?"):
+                children = self.scroll_frame.winfo_children()
+                if children:
+                    children[-1].destroy()
+                    self.save_events()
 
 
 def confirm_action(message):
--- progress.md
+++ progress.md
@@ -163,11 +163,35 @@ Template:
 - What needs improvement? I still need correct small mistakes.
 - What did I learn? What the vals and how to use it, it much easier than we learnied simmilar to vals method before, vals as well allows to make code more clear and readiable for me it's very good because I try to keep all my code as much as possible clear and structured because that important part of maintainability, code should be readiable to fix it if something happend, and already now this structure saved my time a few times, that alllow to find errors faster.
 ---
+## Session [05]
+**Date:** [16.04.2026-17.04.2026]  
+**Time spent:** [about 200 min]  
+**Focus:** [Well this is the bigest session. I did a lot, so 1. I add loading data to app when it starts, 2. I add 1 row which open overview screen already after app starts, because main page this is overview. As well I added sorting teams and individuals to put them on overview mage on leaderbord.  As well add function to inserst leaders in a leaderbord table. Also I did the feature which are quickly show the data and back them to the screen after changing page, this need because each page changing made by destroying screen. Also I add like scaner which check all participants on the page and put them to JSON. 1 of the feature that I like that I learn that lambda, because that very Comfortable feature because before I know how to make coomand which start the function instantly, and the lambda start the function only when the button pressed. As well I know before that I will use it, but now I add this function confirmation message, that not necceary but I add it. As well I learn what mean parent, it's like a screen, basically it's a big element, and children that all small things that are on the screen, boxes, buttons and all this kind of things that, childrens I even was a bit in shocked that coding language has this kind of words.  1 more important thing that I add function which determine date of event to put them in upcoming events, but 1 bug that I have to fix that of you put 1.1.1999 the sytem will put it as a closest event, so it count earliest date instead of date which is closed to today. And last thing that I did, that just fixing stupid spelling mistakes. For this session I prepared a lot, I made a list of what I have to do, then I think how, then learn it, and only them write a code, of course I get a lot of errors because in that session I did a lot of functionality, create UI is much easier then functionality in term of errors. The errors wasn't critical but anyway they take time.]
 
+### Problems / Challenges
+- Almost everything was chalenging
+- 
+
+### Solutions / Actions Taken
+- Wish to finish the main part of app
+- 
+
+### Evidence
+- [Added code]
+- [App workability]
+- 
+- 
+- 
+
+### Reflection
+- What went well? Everything was chalenging but it went well, because in the end I get the xpected result
+- What needs improvement? Sorting by date function.
+- What did I learn? A lot, I writed above.
+---
 # 7. Problems and Fixes
 
 | Problem | Cause | Fix | Status |
-|---|---|---|---|
+|The app put in upcoming event, the event with the earliest date(in total even if the date was 20 years ago)|It could be confusig when app will using|---|Not fixed|
 | UI for Leaderbord and upcoing events doesn't work | Somewhere is mistake | - | Fixed |
 | No scrollbar function in TK Inter frame | Scrollbar are not provided for this part of bibliotek | Switch from tk.frame to tk.canvas for events page | Fixed |
 
